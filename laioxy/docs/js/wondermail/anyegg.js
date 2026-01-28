c43714518e0868cf61c4ecf9f03f161e456fe7f3
Phantazap <46830523+Phantazap@users.noreply.github.com>
2026-01-28 07:59:17 -0600

    Fixed some of the links on site, most will still be broken

--- laioxy/docs/js/wondermail/anyegg.js
+++ laioxy/docs/js/wondermail/anyegg.js
@@ -1,4 +1,4 @@
-import { WonderMail } from '/js/wondermail/password.js';
+import { WonderMail } from '/laioxy/docs/js/wondermail/password.js';
 
 console.log('a');
 $(async function () {
--- laioxy/docs/js/wondermail/mail.js
+++ laioxy/docs/js/wondermail/mail.js
@@ -1,4 +1,4 @@
-import { WonderMail, GetSwapTable } from '/js/wondermail/password.js';
+import { WonderMail, GetSwapTable } from '/laioxy/docs/js/wondermail/password.js';
 
 $(async function () {
   // JSONデータ格納用変数
